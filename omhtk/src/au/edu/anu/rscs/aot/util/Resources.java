8b4aa96aaf02dc83f873516fc750d4228c23c188
Ian Davies <u9007333@anu.edu.au>
2018-12-19 18:12:53 +1100

    add some utils

---
+++ omhtk/src/au/edu/anu/rscs/aot/util/Resources.java
@@ -0,0 +1,88 @@
+/**************************************************************************
+ *  OMHTK - One More Handy Tool Kit                                       *
+ *                                                                        *
+ *  Copyright 2018: Shayne FLint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  OMHTK is a bunch of useful, very generic interfaces for designing     *
+ *  consistent, plus some other utilities. The kind of things you need    *
+ *  in all software projects and keep rebuilding all the time.            *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of OMHTK (One More Handy Tool Kit).                 *
+ *                                                                        *
+ *  OMHTK is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  OMHTK is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with OMHTK.
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
+ *                                                                        *
+ **************************************************************************/
+
+package au.edu.anu.rscs.aot.util;
+
+import java.io.File;
+import java.net.URL;
+
+import au.edu.anu.omhtk.jars.Jars;
+import au.edu.anu.rscs.aot.OmhtkException;
+// not sure about all this!!
+/**
+ * Author Ian Davies
+ *
+ * Date 19 Dec. 2018
+ */
+public class Resources {
+	public static URL getURL(String name) {
+		URL result = ClassLoader.getSystemResource(name);
+		if (new File(result.getFile()).isDirectory()) {
+			throw new OmhtkException("JavaUtils.getClassesInPackage: Directory resources are not permitted '" + name + "'");				
+		}
+		return result;
+	}
+
+	public static URL getURL(String name, String packageName) {
+		String resourceName = packageName.replace('.', Jars.separatorChar) + Jars.separator + name;
+		return getURL(resourceName);
+	}
+
+	public static URL getURL(String name, Class<?> associatedWithClass) {
+		return associatedWithClass.getResource(name);
+	}
+
+	public static File getFile(String name) {
+		URL url = getURL(name);
+		if (url != null)
+			return new File(url.getFile());
+		else
+			return null;
+	}
+
+	public static File getFile(String name, Class<?> associatedWithClass) {
+		URL url = getURL(name, associatedWithClass);
+		if (url != null) {
+			return new File(url.getFile());
+		}
+		else
+			return null;
+	}
+
+	public static File getFile(String name, String packageName) {
+		URL url = getURL(name, packageName);
+		if (url != null)
+			return new File(url.getFile());
+		else
+			return null;
+	}
+
+}
