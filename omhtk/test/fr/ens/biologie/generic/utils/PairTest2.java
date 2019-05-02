26d7d6d9966dd701ee3cb4568e00aa1c9bce2eaa
Ian Davies <ian.davies@anu.edu.au>
2019-05-02 12:25:44 +1000

    module problems

---
+++ omhtk/test/fr/ens/biologie/generic/utils/PairTest2.java
@@ -0,0 +1,14 @@
+package fr.ens.biologie.generic.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PairTest2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
