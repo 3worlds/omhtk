b138cc2b329a0def098f1d7084fa98ea1409d151
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-07-02 14:12:27 +0200

    adding newInstances() to Factory

--- omhtk/src/fr/ens/biologie/generic/Factory.java
+++ omhtk/src/fr/ens/biologie/generic/Factory.java
@@ -1,5 +1,9 @@
 package fr.ens.biologie.generic;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * An interface for factories for objects of class T. Every call to newInstance() returns
  * a new object of class T. cf. {@link Singleton} for unique instances. 
@@ -11,5 +15,13 @@ package fr.ens.biologie.generic;
 public interface Factory<T> {
 	
 	public T newInstance();
+	
+	/** this one may be useful sometimes when instances must be created in a block */
+	public default Collection<T> newInstances() {
+		T instance = newInstance();
+		List<T> list = new ArrayList<>();
+		list.add(instance);
+		return list;
+	}
 
 }
