e30fd9ca555265ecddb4634dc9a3489cdeea70cd
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-10-15 14:22:52 +0200

    updating java compiler to handle whole source directories

--- omhtk/src/fr/ens/biologie/codeGeneration/JavaCompiler.java
+++ omhtk/src/fr/ens/biologie/codeGeneration/JavaCompiler.java
@@ -1,6 +1,7 @@
 package fr.ens.biologie.codeGeneration;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Arrays;
@@ -51,6 +52,28 @@ public final class JavaCompiler {
 		} else
 			return null;
 	}
+	
+	/**
+	 * Compiles all .java files found in the compiler root
+	 * 
+	 * @param compilerRoot the root directory for compilation
+	 * @return error messages, if any
+	 */
+	public final String compileCode(File compilerRoot) {
+		log.info("Compiling all java files in " + compilerRoot.getName() + " ...");
+		String result = compileClass(compilerRoot);
+		if ((result != null) && (!result.isEmpty())) {
+			log.severe("********************************************************************");
+			log.severe("There were compiling errors in " + compilerRoot.getName() + ":");
+			log.severe(result);
+			log.severe("********************************************************************");
+		}
+		log.info("done.");
+		if ((result != null) && (!result.isEmpty()))
+			return result;
+		else
+			return null;
+	}
 
 	/**
 	 * This method compiles one .java file into one .class file.
@@ -87,5 +110,42 @@ public final class JavaCompiler {
 		}
 		return result;
 	}
+	
+		
+	private final String compileClass(File sourceDir) {
+		String result = null;
+		// Must have a compiler available!
+		if (compiler == null)
+			throw new OmhtkException("No complier provided: Please install jdk");
+		if (!sourceDir.isDirectory())
+			throw new OmhtkException("Error: '"+sourceDir+"' is not a directory");
+		FileFilter ff = new FileFilter() {
+			public boolean accept(File pathname) {
+				return pathname.toString().endsWith(".java");
+			}
+		};
+		File[] javaFiles = sourceDir.listFiles(ff);
+		StandardJavaFileManager stdFileManager = compiler.getStandardFileManager(null, Locale.getDefault(), null);		
+		Iterable<? extends JavaFileObject> compilationUnits = stdFileManager
+			.getJavaFileObjectsFromFiles(Arrays.asList(javaFiles));
+		LinkedList<String> options = new LinkedList<String>();
+		options.add("-sourcepath");
+		options.add(sourceDir + File.separator);
+		options.add("-classpath");
+		options.add(System.getProperty("java.class.path"));
+		options.add("-Xlint"); // due to a strange error with DataContainer (usually a warning, actually ??)
+		StringWriter errors = new StringWriter();
+		javax.tools.JavaCompiler.CompilationTask task = compiler.getTask(errors, null, null, options, null,
+			compilationUnits);
+		task.call();
+		result = errors.toString();
+		try {
+			stdFileManager.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return result;
+	}
+
 
 }
