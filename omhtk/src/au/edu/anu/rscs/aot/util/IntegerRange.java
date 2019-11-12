2b5c5763bd74391fbbba8d42044404354ef29b9e
Ian Davies <ian.davies@anu.edu.au>
2019-11-13 10:48:58 +1100

    reparse IntegerRange str

--- omhtk/src/au/edu/anu/rscs/aot/util/IntegerRange.java
+++ omhtk/src/au/edu/anu/rscs/aot/util/IntegerRange.java
@@ -32,7 +32,8 @@ package au.edu.anu.rscs.aot.util;
 import au.edu.anu.rscs.aot.OmhtkException;
 
 /**
- * A property type for multiplicities (i.e. accepts the '*' notation for 'any number').
+ * A property type for multiplicities (i.e. accepts the '*' notation for 'any
+ * number').
  * 
  * @author Shayne Flint - 2012?
  *
@@ -41,11 +42,11 @@ import au.edu.anu.rscs.aot.OmhtkException;
 public class IntegerRange {
 
 	private int first = 0;
-	private int last  = Integer.MAX_VALUE;
+	private int last = Integer.MAX_VALUE;
 
 	public IntegerRange(int first, int last) {
 		this.first = first;
-		this.last   = last;
+		this.last = last;
 		check();
 	}
 
@@ -53,50 +54,69 @@ public class IntegerRange {
 		int idx = str.indexOf("..");
 		if (idx > 0 && (str.length() - idx) > 2) {
 			String startStr = str.substring(0, idx);
-			String endStr   = str.substring(idx+2);
-			first = Integer.valueOf(startStr);
-			if (endStr.equals("*"))
-				last = Integer.MAX_VALUE;
-			else
+			String endStr = str.substring(idx + 2);
+			try {
+				first = Integer.valueOf(startStr);
+			} catch (NumberFormatException e) {
+				first = Integer.MIN_VALUE;
+			}
+			try {
 				last = Integer.valueOf(endStr);
+			} catch (NumberFormatException e) {
+				last = Integer.MAX_VALUE;
+			}
 		} else
 			throw new OmhtkException("'" + str + "' is not a valid integer range");
 		check();
 	}
 
+//	public IntegerRange(String str) {
+//		int idx = str.indexOf("..");
+//		if (idx > 0 && (str.length() - idx) > 2) {
+//			String startStr = str.substring(0, idx);
+//			String endStr   = str.substring(idx+2);
+//			first = Integer.valueOf(startStr);
+//			if (endStr.equals("*"))
+//				last = Integer.MAX_VALUE;
+//			else
+//				last = Integer.valueOf(endStr);
+//		} else
+//			throw new OmhtkException("'" + str + "' is not a valid integer range");
+//		check();
+//	}
+
 	@Override
 	public boolean equals(Object value) {
-		IntegerRange test = (IntegerRange)value;
-		return first==test.first && last==test.last;
+		IntegerRange test = (IntegerRange) value;
+		return first == test.first && last == test.last;
 	}
-	
+
 	private void check() {
 		if (first > last)
 			throw new OmhtkException("Invalid integer range: " + first + " must <= " + last);
 	}
-	
+
 	public boolean inRange(int value) {
 		return (value >= first) && (value <= last);
 	}
-	
+
 	public void check(int value) {
 		if (!inRange(value))
 			throw new OmhtkException(value + " is not in integer range " + first + ".." + last);
 	}
-	
-	
+
 	public static IntegerRange valueOf(String str) {
 		return new IntegerRange(str);
 	}
 
 	private String integerString(int i) {
-		if (i==Integer.MIN_VALUE)
+		if (i == Integer.MIN_VALUE)
 			return "MIN_INTEGER";
-		if (i==Integer.MAX_VALUE)
+		if (i == Integer.MAX_VALUE)
 			return "*";
 		return String.valueOf(i);
 	}
-	
+
 	public String toString() {
 		return integerString(first) + ".." + integerString(last);
 	}
@@ -105,16 +125,16 @@ public class IntegerRange {
 		this.first = first;
 		return this;
 	}
-	
+
 	public int getFirst() {
 		return first;
 	}
-	
+
 	public IntegerRange setLast(int last) {
 		this.last = last;
 		return this;
 	}
-	
+
 	public int getLast() {
 		return last;
 	}
