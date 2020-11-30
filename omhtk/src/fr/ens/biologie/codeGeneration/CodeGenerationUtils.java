9ef0625b5e5e12a45d91a7e83a8ac3620dbd21e0
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-11-30 11:18:39 +0100

    fixed a subtle little bug in code generation

--- omhtk/src/fr/ens/biologie/codeGeneration/CodeGenerationUtils.java
+++ omhtk/src/fr/ens/biologie/codeGeneration/CodeGenerationUtils.java
@@ -7,12 +7,12 @@ import java.io.PrintStream;
 import fr.ens.biologie.generic.JavaCode;
 
 /**
- * 
+ *
  * @author Jacques Gignoux - 4 juil. 2019
  *
  */
 public class CodeGenerationUtils {
- 
+
 	public static void writeFile(JavaCode jc, File file, String className) {
 		file.getParentFile().mkdirs();
 		PrintStream classFile;
@@ -22,9 +22,9 @@ public class CodeGenerationUtils {
 			classFile.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
-	
+
 	public static String checkType(String type) {
 		if (type.equals("Double")) return "double";
 		else if (type.equals("Integer")) return "int";
@@ -34,17 +34,17 @@ public class CodeGenerationUtils {
 		else if (type.equals("Char")) return "char";
 		return type;
 	}
-	
+
 	public static String zero(String type) {
 		if (type.equals("int")) return "0";
 		else if (type.equals("long")) return "0L";
-		else if (type.equals("float")) return "0.0";
-		else if (type.equals("double")) return "0.0";
+		else if (type.equals("float")) return "0.0f";
+		else if (type.equals("double")) return "0.0d";
 		else if (type.equals("boolean")) return "false";
 		else if (type.equals("char")) return "\'\\0\'";
-		else if (type.equals("String")) return "\"\"";		
+		else if (type.equals("String")) return "\"\"";
 		else return "null";
 	}
 
-	
+
 }
