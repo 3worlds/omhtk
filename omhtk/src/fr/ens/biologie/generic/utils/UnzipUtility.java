102a7bcfc9e976fdacf40534a0ff204b27ea88eb
Ian Davies <ian.davies@anu.edu.au>
2021-07-07 13:30:49 +1000

    Unzip utility

---
+++ omhtk/src/fr/ens/biologie/generic/utils/UnzipUtility.java
@@ -0,0 +1,98 @@
+/**************************************************************************
+ *  OMHTK - One More Handy Tool Kit                                       *
+ *                                                                        *
+ *  Copyright 2018: Shayne FLint, Jacques Gignoux & Ian D. Davies         *
+ *       shayne.flint@anu.edu.au                                          *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  OMHTK is a bunch of useful, very generic interfaces for designing     *
+ *  consistent, plus some other utilities. The kind of things you need    *
+ *  in all software projects and keep rebuilding all the time.            *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of OMHTK (One More Handy Tool Kit).                 *
+ *                                                                        *
+ *  OMHTK is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  OMHTK is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with UIT.  If not, see <https://www.gnu.org/licenses/gpl.html>. *
+ *                                                                        *
+ **************************************************************************/
+
+package fr.ens.biologie.generic.utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+ 
+/**
+ * This utility extracts files and directories of a standard zip file to
+ * a destination directory.
+ * @author www.codejava.net
+ *
+ */public class UnzipUtility {
+	   /**
+     * Size of the buffer to read/write data
+     */
+    private static final int BUFFER_SIZE = 4096;
+    /**
+     * Extracts a zip file specified by the inStream to a directory specified by
+     * destDirectory (will be created if does not exists)
+     * @param instream
+     * @param destDirectory
+     * @throws IOException
+     */
+    public void unzip(InputStream inStream, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(inStream);
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdirs();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+    /**
+     * Extracts a zip entry (file entry)
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+         bos.close();
+    }
+
+}
