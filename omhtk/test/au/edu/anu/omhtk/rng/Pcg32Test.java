f071f3df008daa37585b769988557db0ad0b1004
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-04-06 14:45:43 +0200

    updating license header in all files

--- omhtk/test/au/edu/anu/omhtk/rng/Pcg32Test.java
+++ omhtk/test/au/edu/anu/omhtk/rng/Pcg32Test.java
@@ -1,3 +1,34 @@
+/**************************************************************************
+ *  OMHTK - One More Handy Tool Kit                                       *
+ *                                                                        *
+ *  Copyright 2021: Shayne R. Flint, Jacques Gignoux & Ian D. Davies      *
+ *       shayne.flint@anu.edu.au                                          *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  OMHTK is a bunch of useful, very generic interfaces for designing     *
+ *  consistent class hierarchies, plus some other utilities. The kind of  *
+ *  things you need in all software projects and keep rebuilding all the  * 
+ *  time.                                                                 *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of OMHTK (One More Handy Tool Kit).                 *
+ *                                                                        *
+ *  OMHTK is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  OMHTK is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with OMHTK.
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
+ *                                                                        *
+ **************************************************************************/
 package au.edu.anu.omhtk.rng;
 
 import static org.junit.jupiter.api.Assertions.*;
