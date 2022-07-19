256a13bdf9dd1c8194ebcd6e21ad248f69ab831e
Ian Davies <ian.davies@anu.edu.au>
2022-07-20 07:08:35 +1000

    random shuffle alg

--- omhtk/src/fr/ens/biologie/generic/utils/Shuffle.java
+++ omhtk/src/fr/ens/biologie/generic/utils/Shuffle.java
@@ -16,16 +16,29 @@ import au.edu.anu.omhtk.rng.Pcg32;
  */
 public class Shuffle {
 	/**
-	 * Random ordering of n indices.
+	 * Random ordering of n indices. Substitutes {@link Pcg32} for java native
+	 * random generator
 	 * 
 	 * @param n number of items.
-	 * @return a random ordering.
+	 * @return a random ordering of sequential numbers from 0 to n-1 inclusive.
 	 */
 	public static Integer[] shuffleIndices(int n) {
-		Random rnd = new Pcg32();
-		List<Integer> bag = IntStream.rangeClosed(0, n-1).boxed().collect(Collectors.toList());
-		Collections.shuffle(bag);
+		Random rng = new Pcg32();
+		return shuffleIndices(n, rng);
+	}
+
+	/**
+	 * Random ordering of n indices with the given random number generator.
+	 * 
+	 * @param n   number of items.
+	 * @param rng Random number generator.
+	 * @return a random ordering of sequential numbers from 0 to n-1 inclusive.
+	 */
+	public static Integer[] shuffleIndices(int n, Random rng) {
+		List<Integer> bag = IntStream.rangeClosed(0, n - 1).boxed().collect(Collectors.toList());
+		Collections.shuffle(bag, rng);
 		return bag.toArray(new Integer[0]);
+
 	}
 
 }
