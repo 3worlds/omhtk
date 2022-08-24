89bf081297b0d13117e941a89363cc4e48dab360
Ian Davies <ian.davies@anu.edu.au>
2022-08-25 08:56:21 +1000

    some fiddle?

--- omhtk/src/au/edu/anu/rscs/aot/util/Resources.java
+++ omhtk/src/au/edu/anu/rscs/aot/util/Resources.java
@@ -47,20 +47,22 @@ import au.edu.anu.rscs.aot.OmhtkException;
 import fr.ens.biologie.generic.utils.Logging;
 
 /**
- * Utilities to locate resources (usually text files) in the project class hierarchy.
+ * Utilities to locate resources (usually text files) in the project class
+ * hierarchy.
  * 
  * @author Ian Davies - 19 Dec. 2018<br/>
- *		Copied from old code by S. Flint.<br/>
- *      refactored by J. Gignoux 22/5/2019
+ *         Copied from old code by S. Flint.<br/>
+ *         refactored by J. Gignoux 22/5/2019
  */
 // Tested OK with version 0.1.1 on 22/5/2019 - works with jars too.
 public class Resources {
-	
+
 	private static Logger log = Logging.getLogger(Resources.class);
-	
+
 	/**
-	 * Creates a URL for the required resource, based on the SystemClassLoader class path, i.e.
-	 * the class loader running the current application. 
+	 * Creates a URL for the required resource, based on the SystemClassLoader class
+	 * path, i.e. the class loader running the current application.
+	 * 
 	 * @param name the resource to look for
 	 * @return the url, <strong>null</strong> if not found
 	 */
@@ -75,21 +77,25 @@ public class Resources {
 	}
 
 	/**
-	 * Creates a URL based on the required resource name and the associated package name
-	 * @param name the resource to look for (usually, a file)
+	 * Creates a URL based on the required resource name and the associated package
+	 * name
+	 * 
+	 * @param name        the resource to look for (usually, a file)
 	 * @param packageName the package in which the resource is to be found
 	 * @return the URL of the resource, <strong>null</strong> if not found
 	 */
 	public static URL getURL(String name, String packageName) {
-		String resourceName = packageName.replace('.', Jars.separatorChar) 
-			+ Jars.separator + name;
+		String resourceName = packageName.replace('.', Jars.separatorChar) + Jars.separator + name;
 		return getURL(resourceName);
 	}
 
 	/**
-	 * Creates a URL based on the required resource name and the associated class package name.
-	 * @param name the resource to look for (usually, a file)
-	 * @param associatedWithClass the class in which package the resource is to be found
+	 * Creates a URL based on the required resource name and the associated class
+	 * package name.
+	 * 
+	 * @param name                the resource to look for (usually, a file)
+	 * @param associatedWithClass the class in which package the resource is to be
+	 *                            found
 	 * @return the URL of the resource, <strong>null</strong> if not found
 	 */
 	public static URL getURL(String name, Class<?> associatedWithClass) {
@@ -97,9 +103,10 @@ public class Resources {
 	}
 
 	/**
-	 * Returns a file matching the required resource by searching in the application class
-	 * path. 
-	 * <strong>WARNING</strong>: this will not work for resources in jars, that cant be Files
+	 * Returns a file matching the required resource by searching in the application
+	 * class path. <strong>WARNING</strong>: this will not work for resources in
+	 * jars, that cant be Files
+	 * 
 	 * @param name the resource to look for
 	 * @return the File matching the resource, <strong>null</strong> if not found
 	 */
@@ -112,26 +119,29 @@ public class Resources {
 	}
 
 	/**
-	 * Returns a file matching the required resource by searching in the package of the
-	 * <em>Class</em> argument. 
-	 * <strong>WARNING</strong>: this will not work for resources in jars, that cant be Files
-	 * @param name the resource to look for
-	 * @param associatedWithClass the class in which package the resource is to be found
+	 * Returns a file matching the required resource by searching in the package of
+	 * the <em>Class</em> argument. <strong>WARNING</strong>: this will not work for
+	 * resources in jars, that cant be Files
+	 * 
+	 * @param name                the resource to look for
+	 * @param associatedWithClass the class in which package the resource is to be
+	 *                            found
 	 * @return the File matching the resource, <strong>null</strong> if not found
 	 */
 	public static File getFile(String name, Class<?> associatedWithClass) {
 		URL url = getURL(name, associatedWithClass);
 		if (url != null) {
 			return new File(url.getFile());
-		}
-		else
+		} else
 			return null;
 	}
 
 	/**
-	 * Returns a file matching the required resource by searching in the specified package
-	 * <strong>WARNING</strong>: this will not work for resources in jars, that cant be Files
-	 * @param name the resource to look for
+	 * Returns a file matching the required resource by searching in the specified
+	 * package <strong>WARNING</strong>: this will not work for resources in jars,
+	 * that cant be Files
+	 * 
+	 * @param name        the resource to look for
 	 * @param packageName the package in which the resource is to be found
 	 * @return the File matching the resource, <strong>null</strong> if not found
 	 */
@@ -142,7 +152,7 @@ public class Resources {
 		else
 			return null;
 	}
-	
+
 	private static List<String> readText(BufferedReader reader, String name) {
 		List<String> text = new LinkedList<String>();
 		String line = "";
@@ -153,63 +163,66 @@ public class Resources {
 				text.add(line);
 			}
 		} catch (IOException e) {
-			log.severe("Error reading resource "+name);
+			log.severe("Error reading resource " + name);
 			e.printStackTrace();
 		}
 		return text;
 	}
-	
+
 	/**
 	 * This will get a text resource either from a plain file or from a jar
+	 * 
 	 * @param name the resource to look for
 	 * @return the text as a list of Strings, one per line
 	 */
 	public static List<String> getTextResource(String name) {
 		InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
-		if (in==null)
-			throw new OmhtkException("Resource '"+name+"' not found");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in,StandardCharsets.UTF_8));
-		return readText(reader,name);
+		if (in == null)
+			throw new OmhtkException("Resource '" + name + "' not found");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+		return readText(reader, name);
 	}
 
 	/**
-	 * This will get a text resource either from a plain file or from a jar. The resource
-	 * will be searched for in the same package as the <em>Class</em> parameter.
-	 * @param name the resource to look for
-	 * @param associatedWithClass the class in which package the resource is to be found
+	 * This will get a text resource either from a plain file or from a jar. The
+	 * resource will be searched for in the same package as the <em>Class</em>
+	 * parameter.
+	 * 
+	 * @param name                the resource to look for
+	 * @param associatedWithClass the class in which package the resource is to be
+	 *                            found
 	 * @return the text as a list of Strings, one per line
 	 */
 	public static List<String> getTextResource(String name, Class<?> associatedWithClass) {
 		InputStream in = associatedWithClass.getResourceAsStream(name);
-		if (in==null)
-			throw new OmhtkException("Resource '"+name+"' not found");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in,StandardCharsets.UTF_8));
-		return readText(reader,name);
+		if (in == null)
+			throw new OmhtkException("Resource '" + name + "' not found");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+		return readText(reader, name);
 	}
-	
+
 	/**
 	 * @param name package relative resource file (dot separated)
 	 * @return fully qualified File obtained by the ClassLoader
 	 */
 	public static File getPackagedFile(String name) {
 		int idx = name.lastIndexOf('.');
-		String fname = new File( name.substring(0, idx).replace(".","/")
-		        + name.substring(idx)).getName();
-		String packageName = name.replace("."+fname, "");
-		return getFile(fname,packageName);
+		String fname = new File(name.substring(0, idx).replace(".", "/") + name.substring(idx)).getName();
+		String packageName = name.replace("." + fname, "");
+		return getFile(fname, packageName);
 	}
 
 	/**
-	 * returns a name in the form: 	au.edu.anu.twcore.archetype.tw.3wA-Experiment.ugt<br/>
-	 * as a name in the form:		au/edu/anu/twcore/archetype/tw/3wA-Experiment.ugt<br/>
-	 * which can be directly used as a resource name in the other methods of this class.
-	 * @param name
-	 * @return
+	 * Convert a dot separated string to a forward slash ('/') separated string(?).
+	 * NB this is no OS independent!
+	 * 
+	 * @return Forward slash separated string.
+	 * @param name a dot separated package name.
+	 * 
 	 */
 	public static String getPackagedFileName(String name) {
 		int idx = name.lastIndexOf('.');
-		String fname = name.substring(0, idx).replace(".","/")
-		        + name.substring(idx);
+		String fname = name.substring(0, idx).replace(".", "/") + name.substring(idx);
 		return fname;
 	}
 
